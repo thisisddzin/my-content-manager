044347307ba72c176e3f734fd7b11f7790959cce
Adenilson Santos <adenilsonblog@gmail.com>
2018-11-05 12:15:18 -0300

    Sistema de gerenciamento completo, do meu site.

--- meusite/obs - arquivo mysqli - BD/mrgeekti.sql
+++ meusite/obs - arquivo mysqli - BD/mrgeekti.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100136
 File Encoding         : 65001
 
-Date: 2018-11-02 19:35:00
+Date: 2018-11-05 12:06:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `produtos` (
   `responsivoproduto` varchar(255) DEFAULT NULL,
   `datapublicacao` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`produtoID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of produtos
@@ -77,10 +77,11 @@ CREATE TABLE `usuarios` (
   `admin` tinyint(1) NOT NULL,
   `foto` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`usuarioID`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of usuarios
 -- ----------------------------
-INSERT INTO `usuarios` VALUES ('1', 'adenilsonblog@gmail.com', 'adenilson201', '87142589', '28/10/18 13:25:32', '1', '_images/publicadas/photo_2018_305181021_9061648058DQ11B7TOAG42MUIH3N5.jpg');
+INSERT INTO `usuarios` VALUES ('1', '', 'admin', 'admin', null, '1', '_images/publicadas/meuperfil.jpg');
+INSERT INTO `usuarios` VALUES ('8', '', 'visitante', 'visitante', '03/11/18 10:32:34', '0', '_images/user_default.png');
 SET FOREIGN_KEY_CHECKS=1;
